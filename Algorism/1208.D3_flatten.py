26e15303a9e7dff222c840436f1f0a51795d55c9
Ak-mong <jeabin1129@naver.com>
2024-01-31 00:13:09 +0900

    add algorism

--- Algorism/1208.D3_flatten.py
+++ Algorism/1208.D3_flatten.py
@@ -6,13 +6,67 @@ T = 10
 for tc in range(1, T + 1):
     N = int(input())
     arr = list(map(int,input().split()))
-    cnt = [0] * len(arr)
-    print(max(arr))
-    print(arr)
+    # 평탄화 작업 반복문
+    for _ in range(N):
+        max_i = 0
+        min_i = 0
+        for i in range(len(arr)):
+            if arr[max_i] < arr[i]:
+                max_i = i
+            if arr[min_i] > arr[i]:
+                min_i = i
+        arr[max_i] -= 1
+        arr[min_i] += 1
+
+    # 평탄화가 완료 된 후 max 랑 min 찾기가 한번 더 이루어져야 함
     for i in range(len(arr)):
-        cnt[arr[i]] += 1
-    print(cnt)
-    # print(f'#{tc} {sum_v}')
+        if arr[max_i] < arr[i]:
+            max_i = i
+        if arr[min_i] > arr[i]:
+            min_i = i
+    print(f'#{tc} {arr[max_i] - arr[min_i]}')
+"""
+    max_v = 0
+    min_v = 101
+    min_i = -1
+    max_i = -1
+    for i in range(len(arr)):
+        if max_v < arr[i]:
+            max_v = arr[i]
+            max_i = i
+        if min_v > arr[i]:
+            min_v = arr[i]
+            min_i = i
+    x = 0
+    while x < N:
+        arr[max_i] -= 1
+        arr[min_i] ++ 1
+        x += 1
+
+    print(f'#{tc} {arr[max_i] - arr[min_i]}')
+    """
+"""    cnt = [0] * 101
+    # print(arr)
+    for i in arr:
+        cnt[i] += 1
+    max_v = min_v = arr[1]
+    for i in arr:
+        if max_v < i:
+            max_v = i
+        if min_v > i:
+            min_v = i
+    x = 0
+    while  x< N:
+        cnt[max_v] -= 1
+        cnt[min_v] += 1
+        if cnt[max_v] == 0:
+            max_v -= 1
+        if cnt[min_v] == N:
+            min_v += 1
+        x +=1
+    print(max_v - min_v)
+    """
+
 
 
 
--- Algorism/main.py
+++ Algorism/main.py
@@ -1,21 +1,27 @@
 import sys
-sys.stdin = open("txt/list_sum.txt", "r")
+sys.stdin = open("txt/flatten", "r")
 
-def print_hi(name):
-    t = int(input())  # 상자가 쌓여있는 가로 길이
+for tc in range(1, 11):
+    dump_cnt = int(input())
+    floor_list = list(map(int, input().split()))
 
-    for tc in range(t):
-        n, m = map(int, input().split())  # 상자가 쌓여있는 가로 길이
+    for _ in range(dump_cnt):
+        max_index = 0
+        min_index = 0
 
-        numbers = list(map(int, input().split()))
-        lst = [sum(numbers[:m])]
-        for i in range(1, n - m + 1):
-            _sum = lst[i - 1] - numbers[i - 1] + numbers[i + m - 1]
-            lst.append(_sum)
+        for num in range(len(floor_list)):
+            if floor_list[max_index] < floor_list[num]:
+                max_index = num
+            if floor_list[min_index] > floor_list[num]:
+                min_index = num
 
-        print(f'#{tc + 1} {max(lst) - min(lst)}')
+        floor_list[max_index] -= 1
+        floor_list[min_index] += 1
 
+    for num in range(len(floor_list)):
+        if floor_list[max_index] < floor_list[num]:
+            max_index = num
+        if floor_list[min_index] > floor_list[num]:
+            min_index = num
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    print(f"#{tc} {floor_list[max_index] - floor_list[min_index]}")
