b8ca4a0744efd53ef938986ff017d06b583fb396
Ak-mong <jeabin1129@naver.com>
2024-02-23 01:09:02 +0900

    algorism

--- Algorism/test.py
+++ Algorism/test.py
@@ -1,15 +1,24 @@
 import sys;sys.stdin =open('input.txt')
-def inorder(node):
-    if node <= n:
-        inorder(2*node)
-        ans.append(tmp[node])
-        inorder(2*node+1)
+
+def maketree(node):
+    global num
+    # 왼 현 오
+    if node <= n: # node가 n보다 작은 수 일때만
+        maketree(node *2) #왼
+        tree[node] = num # 현
+        num += 1 # node에 들어갈 수가 1~6이기 때문
+        maketree(node * 2 + 1)# 오
+
+
+
 t = int(input())
 for tc in range(1,t+1):
-    e, n = map(int,input().split())
-    arr = list(map(int,input().split()))
-    tree = [''] * (n+1)
-    num = 1
-    ans = []
-    inorder(1)
-    print(ans)
+    n = int(input())
+    # 루트 노드 : 1번 노드, 노드 != 값
+    tree = [0] * (n+1) # 트리의 길이
+    # print(tree)
+    num = 1 # 완전트리임으로 사용
+    maketree(1) # 시작 노드가 1번 노드
+    print(f'#{tc} {tree[1]} {tree[n//2]}')
+
+
