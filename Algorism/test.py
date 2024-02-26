5a69728f25d66cf46b1f2210b4a07832459f1a63
Ak-mong <jeabin1129@naver.com>
2024-02-26 23:43:13 +0900

    algorism

--- Algorism/test.py
+++ Algorism/test.py
@@ -1,23 +1,22 @@
 import sys;sys.stdin =open('input.txt')
 
-def makeTree(node):
-    global num
-    # 재귀로 할 거다.
-    if node <= n:
-        #후위
-        makeTree(node*2) #왼쪽 자식
+def get_result():
+    cnt = 0
+    for i in range(n): # arr 한바퀴 도는것
+        i_b = arr[i][1]
+        for j in range(i): # i 와 그 전 인덱스들을 비교하겠다
+            j_b = arr[j][1]
+            if i_b < j_b: # A 전봇대에서는 위에 있는 전선이 B에서는 더 작을 때
+                cnt += 1
+    return cnt
 
-        makeTree(node*2+1) #오른 쪽 자식
-        tree[node] = arr[num]  # 부모
-        num += 1
 t = int(input())
 for tc in range(1,t+1):
     n = int(input())
-    arr = list(map(int,input().split()))
-    # print()
-    # 이진 최소값 구하기 => 재귀
-    # 일단 tree선언
-    tree = [0] * (n+1)
-    num = 0 # 노드 인덱스에 들어갈 arr[num] 값
-    makeTree(1) # 노드 번호는 루트가 1
-    print(tree)
+    arr = [tuple(map(int,input().split())) for _ in range(n)]
+    # print(arr)
+    # arr.sort(key=lambda x:x[0])
+    arr.sort(key=lambda x:x[0])
+    # print(arr)
+    result = get_result()
+    print(f'#{tc} {result}')
