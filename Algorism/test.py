10da5addca765b793fa0a930374458c0d7c033b8
jaebin <jeabin1129@naver.com>
2024-03-12 18:00:24 +0900

    update

--- Algorism/test.py
+++ Algorism/test.py
@@ -2,37 +2,29 @@ import sys;
 sys.stdin =open('input.txt')
 import time
 # starttime = time.time()
-# 1094 막대기 실버5
+# 실버 5
+N,K = map(int,input().split())
 
-'''
-64
-32 16 8 4 2 1
-지민이가 가지고 있는 막대의 길이를 모두 더한다. 
-처음에는 64cm 막대 하나만 가지고 있다. 
-이때, 합이 X보다 크다면, 아래와 같은 과정을 반복한다.
-가지고 있는 막대 중 길이가 가장 짧은 것을 절반으로 자른다.
-만약, 위에서 자른 막대의 절반 중 하나를 버리고 
-남아있는 막대의 길이의 합이 X보다 크거나 같다면, 
-위에서 자른 막대의 절반 중 하나를 버린다.
-이제, 남아있는 모든 막대를 풀로 붙여서 Xcm를 만든다.
-'''
-stick = 64
-stick_arr = []
-
-x = int(input())
-while stick > 0:
-    stick_arr.append(stick)
-    stick //= 2
-    # stick_arr.append(stick)
-i = 0
-sum_v = 0
+arr = [list(map(int,input().split())) for _ in range(N)]
+# arr.sort(key=lambda x : (x[1],x[2],x[3]),reverse=True)
+new_arr = []
+for i in arr:
+    sum_v = i[1] * 3 + i[2]* 2 + i[3]* 1
+    new_arr.append([i[0],sum_v])
+new_arr.sort(key=lambda x : x[1],reverse=True)
+# 같은 등수일 때 어떻게 표현할까
 cnt = 0
-while i<len(stick_arr):
-    if stick_arr[i] > x:
-        i += 1
-    else:
-        x -= stick_arr[i]
+cnt2 = 0
+for i in range(len(new_arr)):
+    if new_arr[i][1] != new_arr[i-1][1]:
         cnt += 1
+    else:
+        cnt2 += 1
+    new_arr[i].append(cnt)
+for x in new_arr:
+    if x[0] == K:
+        print(x[2])
+# print(new_arr)
 
-# print(stick_arr)
-print(cnt)
+
+
