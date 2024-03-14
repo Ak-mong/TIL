d3eedff9077548bb11aec7dc0ea8dcdda3bda9ba
Ak-mong <jeabin1129@naver.com>
2024-03-15 01:06:19 +0900

    update

--- Algorism/test.py
+++ Algorism/test.py
@@ -2,31 +2,16 @@ import sys;
 sys.stdin =open('input.txt')
 import time
 # starttime = time.time()
-# 실버 5
-# N,K = map(int,input().split())
-#
-# arr = [list(map(int,input().split())) for _ in range(N)]
-# # arr.sort(key=lambda x : (x[1],x[2],x[3]),reverse=True)
-# new_arr = []
-# for i in arr:
-#     sum_v = i[1] * 3 + i[2]* 2 + i[3]* 1
-#     new_arr.append([i[0],sum_v])
-# new_arr.sort(key=lambda x : x[1],reverse=True)
-# # 같은 등수일 때 어떻게 표현할까
-# cnt = 0
-# cnt2 = 0
-# for i in range(len(new_arr)):
-#     if new_arr[i][1] != new_arr[i-1][1]:
-#         cnt += 1
-#     else:
-#         cnt2 += 1
-#     new_arr[i].append(cnt)
-# for x in new_arr:
-#     if x[0] == K:
-#         print(x[2])
-# # print(new_arr)
-#
-#
-#
-dicts = {'author':'fda', 'title':'fdafda'}
-print(dicts['author'])
+# 너의 평점은 실버 5 25206
+arr = [list(sys.stdin.readline().split()) for _ in range(20)]
+grade = {'A+': 4.5, 'A0': 4.0,'B+': 3.5,'B0': 3.0,'C+': 2.5,'C0': 2.0,'D+': 1.5,'D0': 1.0,'F': 0.0}
+sum_v = 0
+score_v = 0
+for i in arr:
+    score = float(i[1]) #
+    if i[2] == 'P': continue
+    grade_v = grade[i[2]]
+    sum_v += score * grade_v
+    score_v += score
+print(sum_v/score_v)
+# print(arr)
