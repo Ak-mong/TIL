1a47b50a684a4a893232c9078f41c998a360bbc5
Jae Bin Lee <jeabin1129@naver.com>
2024-03-25 18:00:20 +0900

    240325

--- Algorism/test.py
+++ Algorism/test.py
@@ -1,38 +1,20 @@
 import sys;
 sys.stdin =open('input.txt')
-import time
-# starttime = time.time()
-# from collections import deque
-# def bfs(a): # a를 b로 바꿔야된다
-#     que = deque()
-#     que.append(a)
-#
-#     while que:
-#
-# n,m = map(int,input().split())
-#
-# bfs(n)
-import time
-starttime = time.time()
-from collections import deque
-def bfs(n,m):
-    Q = deque()
-    Q.append(n)
-    visited[n] = 1
-    while Q:
-        v = Q.popleft()
-        if v > m: continue
-        if v==m:
-            return visited[m]
-        for w in [v*2,int(str(v)+'1')]:
-            if 0< w <= m:# 백만 이하의 자연수
-                if visited[w] ==0:
-                    visited[w] = visited[v] + 1
-                    Q.append(w)
-    return -1
+
+# 15652 N 과 M (4) 실버 3
+# 중복 순열, 오른차순
+def perm(level):
+    if len(path) == m:
+        print(*path)
+        return
+    for i in range(level,n+1):
+        path.append(i)
+        perm(i)
+        path.pop()
 
 
-N,M = map(int,input().split())
-visited = [0] * (M + 1)
-print(bfs(N,M))
-print(time.time()-starttime)
+n,m = map(int,input().split())
+path = []
+perm(1)
+
+
