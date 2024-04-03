44a61a2f1aae0f5028eb48ed5b25080a903bdb01
Jae Bin Lee <jeabin1129@naver.com>
2024-04-03 17:57:05 +0900

    240403

--- Algorism/test.py
+++ Algorism/test.py
@@ -1,14 +1,33 @@
 import sys
 from collections import deque
 sys.stdin =open('input.txt')
+input = sys.stdin.readline
+# 1389 케빈 베이컨의 6단계 법칙 실버1
+from collections import deque
 
-# 5639 이진 검색 트리 골드5
-# 전위 순회 결과를 보고 후위순회 결과를 만들어라
+import sys
+input = sys.stdin.readline
+# 4803  트리 골드 4
+n,m= map(int,input().split())
+# n 정점의 개수, m 간선의 개수
+adjl = [[] for _ in range(n+1)]
+for i in range(m):
+    s,e = map(int,input().split())
+    adjl[s].append(e)
+    adjl[e].append(s)
 
-lines = sys.stdin.readline()
-# print(lines)
-# print(lines)
-for line in lines:
-    print(line.split())
-    # value = int(line)
-    # print(value)
+print(adjl)
+path = []
+visited = [0] * (n+1)
+visited[1] = 1
+def dfs(level):
+    if len(path) == n:
+        print(path)
+    for i in adjl[level]:
+        if visited[i]: continue
+        path.append(i)
+        dfs(i)
+        path.pop()
+
+
+dfs(1)
