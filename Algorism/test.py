33169b6b4dbf4064dc9fa8bbb736327cd9278ffa
Ak-mong <jeabin1129@naver.com>
2024-04-05 01:16:46 +0900

    algorism
    algorism

--- Algorism/test.py
+++ Algorism/test.py
@@ -2,48 +2,59 @@ import sys
 from collections import deque
 sys.stdin =open('input.txt')
 input = sys.stdin.readline
-# sys.setrecursionlimit(10**6)
-# 1931 회의실 배정 실버 1
-n = int(input())
+#sys.setrecursionlimit(10**6)
+
+# 20364 부동산 다툼 실버1
+# 이진트리
+def maketree(node):
+    if node > n:
+        return
+    parent = node // 2
+    leftchild = node * 2
+    rightchild = node * 2 + 1
+    tree[node][0] = parent
+    tree[node][1] = leftchild
+    tree[node][2] = rightchild
+    maketree(leftchild)
+    maketree(rightchild)
+def goland(node):
+    global result
+    deq = deque()
+    deq.append(node)
+    while deq:
+        value = deq.popleft()
+        # if value == land:
+        #     visited[value] = 1
+        #     return
+        for child in tree[value][1:]:
+            if child > n: continue
+            if child == land:
+                # 이 부분 고치기
+                result = value
+                return
+                visited[child] = 1
+                result = 0
+                return
+            ###################
+            deq.append(child)
+
+n,q = map(int,input().split())
 arr = []
-for _ in range(n):
-    arr.append(list(map(int,input().split())))
-arr.sort(key=lambda x : x[1])
-print(arr)
-s = arr[0][0]
-e = arr[len(arr)-1][1]
-print(s,e)
-index = 0
-while s < e:
-     s = arr[index][1]
+tree = [[0] * 3 for _ in range(n+1)]
+# parent = 0, left = 1, right = 2
+visited = [0] * (n+1)
+result = 0
+maketree(1)
+for i in range(1,q+1):
+    # arr.append((i,int(input())))
+    land = int(input())
+    goland(1)
+    # print(i,visited)
+    # print(i,tree)
+    print(result)
 
 
-
-
-
-
-
-# # 4803  트리 골드 4
-# n,m= map(int,input().split())
-# # n 정점의 개수, m 간선의 개수
-# adjl = [[] for _ in range(n+1)]
-# for i in range(m):
-#     s,e = map(int,input().split())
-#     adjl[s].append(e)
-#     adjl[e].append(s)
-#
-# print(adjl)
-# path = []
-# visited = [0] * (n+1)
-# visited[1] = 1
-# def dfs(level):
-#     if len(path) == n:
-#         print(path)
-#     for i in adjl[level]:
-#         if visited[i]: continue
-#         path.append(i)
-#         dfs(i)
-#         path.pop()
-#
-#
 # dfs(1)
+# print(tree)
+# for i in range(q):
+#     sors, land = map(arr[i])
