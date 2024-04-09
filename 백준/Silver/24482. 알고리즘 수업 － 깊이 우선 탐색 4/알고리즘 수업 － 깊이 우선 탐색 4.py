7e27579f18c4ed0bbd80aeeea9881cb4aee55eeb
Jae Bin Lee <50922212+Ak-mong@users.noreply.github.com>
2024-04-10 00:55:57 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 4, Time: 576 ms, Memory: 68780 KB -BaekjoonHub

---
+++ 백준/Silver/24482. 알고리즘 수업 － 깊이 우선 탐색 4/알고리즘 수업 － 깊이 우선 탐색 4.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+# 깊이 우선 탐색 4 실버 2
+n,m,r = map(int,input().split())
+adjl = [[] for _ in range(n+1)]
+visited = [False] * (n+1)
+depth = [0] * (n+1)
+for _ in range(m):
+    s,e = map(int,input().split())
+    adjl[s].append(e)
+    adjl[e].append(s)
+def dfs(level):
+    if visited[level]: return
+    visited[level] = True
+    adjl[level].sort(reverse=True)
+    for i in adjl[level]:
+        if visited[i]: continue
+        depth[i] = depth[level] + 1
+        dfs(i)
+dfs(r)
+for p in range(1,n+1):
+    if visited[p]:
+        print(depth[p])
+    else:
+        print(-1)
