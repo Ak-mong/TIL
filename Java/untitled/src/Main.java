ecd0c4f7ceb6fe081c3a3e98d74eef60334688d2
이재빈 <jeabin1129@naver.com>
2024-08-17 23:38:14 +0900

    240817_M Java/.obsidian/workspace.json Java/untitled/src/Main.java, A Java/untitled/.idea/.gitignore Java/untitled/.idea/misc.xml Java/untitled/.idea/modules.xml Java/untitled/.idea/vcs.xml Java/untitled/untitled.iml
    Affected files:
    Java/.obsidian/workspace.json
    Java/untitled/.idea/.gitignore
    Java/untitled/.idea/misc.xml
    Java/untitled/.idea/modules.xml
    Java/untitled/.idea/vcs.xml
    Java/untitled/src/Main.java
    Java/untitled/untitled.iml

--- Java/untitled/src/Main.java
+++ Java/untitled/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        int N = 6;
+        int[] resultArray= new int[5]
+                
         System.out.println("Hello world!");
     }
 }
