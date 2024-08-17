4ef80b94dcd38bac3c4500e507061cd7cb202bee
이재빈 <jeabin1129@naver.com>
2024-08-17 23:38:50 +0900

    240817_M Java/untitled/src/Main.java
    Affected files:
    Java/untitled/src/Main.java

--- Java/untitled/src/Main.java
+++ Java/untitled/src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         int N = 6;
-        int[] resultArray= new int[5]
-                
+        int[] resultArray= new int[5];
+        for (int i=0; i < resultArray.length; i++){
+            resultArray[i] = (int) (Math.random() + N) +1;
+            
+        }
+
         System.out.println("Hello world!");
     }
 }
